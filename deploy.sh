8cb0058c0e57feb558afc6b1f5ec85289576a4a7
A.Darbandi <darbandi1996@gmail.com>
2021-01-13 17:29:51 +0330

    worked on okteto

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# curl https://get.okteto.com -sSfL | sh
+okteto login --token=QdgToMzqRaOGoBUpzWK29vbAukBzx6haFPjueZv8
+okteto namespace mr-exception
+okteto build -t registry.cloud.okteto.net/mr-exception/bell: .
+registry.cloud.okteto.net/mr-exception/bell
