dfa59bec149e04f7b3b3ae0548b234289242781b
alireza darbandi <darbandi1996@gmail.com>
2021-01-13 21:22:25 +0330

    added okteto

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# curl https://get.okteto.com -sSfL | sh
-okteto login --token=QdgToMzqRaOGoBUpzWK29vbAukBzx6haFPjueZv8
+curl https://get.okteto.com -sSfL | sh
 okteto namespace mr-exception
 okteto build -t registry.cloud.okteto.net/mr-exception/bell .
