acfccdb682df67d21db702d73a202854c3d3ca9e
alireza darbandi <darbandi1996@gmail.com>
2021-01-16 02:03:49 +0330

    worked on configts implement env

--- config.ts
+++ config.ts
@@ -1,11 +1,13 @@
 import { IApplicationConfig } from "./interfaces/config";
-const config: IApplicationConfig[] = [
-  // sparow application
-  {
-    name: "sparow",
-    auth_token: "test-auth-token",
-    base_url: "http://sparow_core/api",
-  },
-];
-
+const config: IApplicationConfig[] = process.env.CONFIGS.split("@").map(
+  (conf) => {
+    const parts = conf.split(",");
+    return {
+      name: parts[0],
+      auth_token: parts[1],
+      base_url: parts[2],
+    };
+  }
+);
+console.log(config);
 export default config;
