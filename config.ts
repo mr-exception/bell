1294f344a06c28f8323ef874e29a647bc023dc82
alireza darbandi <darbandi1996@gmail.com>
2021-01-16 01:12:29 +0330

    added config.ts

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@ const config: IApplicationConfig[] = [
   {
     name: "sparow",
     auth_token: "test-auth-token",
-    base_url: "http://core:5000/api",
+    base_url: "http://sparow_core/api",
   },
 ];
 
