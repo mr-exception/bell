33020a12917d66ca65ac29c10ab3226cecfa702d
alireza darbandi <darbandi1996@gmail.com>
2021-01-16 10:55:56 +0330

    error handling in config.ts

--- config.ts
+++ config.ts
@@ -1,12 +1,15 @@
 import { IApplicationConfig } from "./interfaces/config";
-const config: IApplicationConfig[] = process.env.CONFIGS.split("@").map(
-  (conf) => {
-    const parts = conf.split(",");
+const config: IApplicationConfig[] = (process.env.CONFIGS || "")
+  .split("@")
+  .map((conf) => {
+    const parts = (conf || "").split(",");
+    if (parts.length !== 3) return undefined;
     return {
       name: parts[0],
       auth_token: parts[1],
       base_url: parts[2],
     };
-  }
-);
+  })
+  .filter((c) => c);
+console.log(config);
 export default config;
