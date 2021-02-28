429699d455ca412b2b49910cdcd1d0960681c3c2
mr-exception <darbandi1996@gmail.com>
2021-03-01 02:25:19 +0330

    removed config.ts

---
+++ config.example.ts
@@ -0,0 +1,9 @@
+import { IApplicationConfig } from "./interfaces/config";
+const config: IApplicationConfig[] = [
+  {
+    name: "sparow",
+    auth_token: "test-token",
+    base_url: "core.sparow.salimon.ir",
+  },
+];
+export default config;
